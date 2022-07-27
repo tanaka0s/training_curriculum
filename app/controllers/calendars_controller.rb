4e4c30e8adc4635be87b67cf23f820a371968386
tanaka0s <arima131409@gmail.com>
2022-07-27 20:36:59 +0900

    19行目の修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -16,7 +16,7 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:plan).permit(:date, :plan, :wday)
+    params.require(:plan).permit(:date, :plan)
   end
 
   def getWeek
